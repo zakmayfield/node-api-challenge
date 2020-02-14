363d2812fd8b4afb8b7a7f5c26a9ca643e7a4a49
Zachary Mayfield <zakmayfield@gmail.com>
2020-02-14 11:40:43 -0500

    implemented CRUD for projects

---
+++ data/helpers/projectRouter.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const Projects = require('./projectModel');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  Projects.get()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: "error on our end, sorry" });
+    })
+})
+
+router.post('/', (req, res) => {
+  Projects.insert(req.body)
+    .then(() => {
+      res.status(201).json(req.body);
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: "error on our end, sorry" })
+    })
+})
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+
+  Projects.remove(id)
+    .then(() => {
+      res.status(202).json({ message: "user successfully removed" });
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: "error on our end, sorry" });
+    })
+})
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const payload = req.body;
+
+  Projects.update(id, payload)
+    .then(() => {
+      res.status(201).json({message: "user updated"})
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({ error: "error on our end, sorry" });
+    })
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const server = express();
 const morgan = require('morgan');
 const cors = require('cors');
 
+const projectRouter = require('./data/helpers/projectRouter');
+const actionRouter = require('./data/helpers/actionRouter');
+
 //-----middleware------
 server.use(express.json())
 server.use(cors()); // use react app with server
@@ -17,6 +20,9 @@ function logger(req, res, next) {
 }
 
 //-----routes-----
+server.use('/api/projects', projectRouter);
+server.use('/api/actions', actionRouter);
+
 
 server.get('/', logger, (req, res) => {
   res.send(`<h2>Server's up n' runnin'</h2>`);
